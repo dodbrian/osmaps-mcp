7a9e0ee1cfcd57a01e3758c8f38457695f49d6c7
Denis Zimin <roydronov@gmail.com>
2026-03-12 20:44:49 +0100

    Add project configuration files

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      'no-console': 'error'
+    }
+  }
+);
